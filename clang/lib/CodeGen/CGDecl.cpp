0889de893910e92d87f194db0cd2aa3cb0d3a1cd
Andrej Jakovljevic <andrej.jakovljevic2000@gmail.com>
2023-04-14 20:17:51 +0200

    l

--- clang/lib/CodeGen/CGDecl.cpp
+++ clang/lib/CodeGen/CGDecl.cpp
@@ -1652,17 +1652,14 @@ CodeGenFunction::EmitAutoVarAlloca(const VarDecl &D) {
 
   llvm::errs() << "end of emission\n";
   // Add metadata for sizeof
-  if (is_ptr)
+  if (is_ptr && address)
   {
-    llvm::Type* AllocaTy = getTypes().ConvertTypeForMem(D.getType());
-    llvm::AllocaInst *Alloc =
-      CreateTempAlloca(AllocaTy, D.getNameAsString());
     auto MD = llvm::MDString::get(getLLVMContext(), "intptr_t");
     llvm::MDNode* MDNode = llvm::MDNode::get(getLLVMContext(), MD);
     std::vector<llvm::Metadata*> metadata_v;
     metadata_v.push_back(MDNode);
     ArrayRef<llvm::Metadata*> arr(metadata_v);
-    Alloc->addMetadata_public("sizeof", *llvm::MDNode::get(getLLVMContext(), arr));
+    address->addMetadata_public("sizeof", *llvm::MDNode::get(getLLVMContext(), arr));
   }
   return emission;
 }
