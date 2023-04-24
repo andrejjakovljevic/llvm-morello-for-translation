d09563998234ea7f0683470459bc291295bf71c9
Andrej Jakovljevic <andrej.jakovljevic2000@gmail.com>
2023-04-24 19:23:51 +0200

    l

--- clang/lib/CodeGen/CGCall.cpp
+++ clang/lib/CodeGen/CGCall.cpp
@@ -2931,7 +2931,7 @@ void CodeGenFunction::EmitFunctionProlog(const CGFunctionInfo &FI,
         llvm::errs() << "name=" << Arg->getName() << "\n";
         llvm::errs() << "type=" << Arg->getType().getAsString() << "\n";
         llvm::errs() << "AM I coercing3?\n";
-        llvm::StringRef attr_name = "coarced#"+Arg->getName().str();
+        llvm::StringRef attr_name = Arg->getName().str();
         llvm::AttributeList Attrs = Fn->getAttributes();
         AI->setName(Arg->getName() + ".coerce");
         llvm::AttrBuilder AB;
